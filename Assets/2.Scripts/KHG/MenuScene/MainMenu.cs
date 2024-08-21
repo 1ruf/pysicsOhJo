c5ce842e1f9a18466c1aa0192034a972d7c60e55
1_ruf <gyeol0411@naver.com>
2024-08-21 15:17:12 +0900

    AddMenu
    add menu intro animation

---
+++ Assets/2.Scripts/KHG/MenuScene/MainMenu.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Image background,BlockFrame;
+    [SerializeField] private GameObject playBtn, settingBtn, quitBtn, mainTitle;
+    private RectTransform title, pBtn, sBtn, qBtn;
+    private int btnStartY = -800, titleStartY = 1000;
+    private void Awake()
+    {
+        title = mainTitle.GetComponent<RectTransform>();
+        pBtn = playBtn.GetComponent<RectTransform>();
+        sBtn = settingBtn.GetComponent<RectTransform>();
+        qBtn = quitBtn.GetComponent<RectTransform>();
+    }
+    private void Start()
+    {
+        StartCoroutine(BtnArise());
+    }
+    private void SetStartPos()
+    {
+        title.anchoredPosition = new Vector2(0, titleStartY);
+        pBtn.anchoredPosition = new Vector2(pBtn.anchoredPosition.x, btnStartY);
+        sBtn.anchoredPosition = new Vector2(sBtn.anchoredPosition.x, btnStartY);
+        qBtn.anchoredPosition = new Vector2(qBtn.anchoredPosition.x, btnStartY);
+        background.DOFade(0, 0);
+    }
+    private IEnumerator BtnArise()
+    {
+        SetStartPos();
+        title.DOMoveY(750, 2).SetEase(Ease.OutBounce);
+        yield return new WaitForSeconds(2f);
+        background.DOFade(1, 1);
+        yield return new WaitForSeconds(0.7f);
+        pBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(0.2f);
+        sBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(0.2f);
+        qBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+    }
+    private IEnumerator BtnDisapper()
+    {
+        title.DOMoveY(1500, 1).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.2f);
+        pBtn.DOMoveY(btnStartY, 1).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.2f);
+        sBtn.DOMoveY(btnStartY, 1).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.2f);
+        qBtn.DOMoveY(btnStartY, 1).SetEase(Ease.InBack);
+    }
+    private IEnumerator BtnReArise()
+    {
+        title.DOMoveY(750, 2);
+        yield return new WaitForSeconds(0.7f);
+        pBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(0.2f);
+        sBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(0.2f);
+        qBtn.DOMoveY(260, 1).SetEase(Ease.OutBack);
+    }
+    private IEnumerator OpenMainScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+        BlockFrame.DOFade(1, 1);
+        yield return new WaitForSeconds(1.2f);
+        SceneManager.LoadScene("MainScene");
+    }
+    public void StartBtnClicked()
+    {
+        StartCoroutine(BtnDisapper());
+        StartCoroutine(OpenMainScene());
+    }
+    public void SettingBtnClicked()
+    {
+        StartCoroutine(BtnDisapper());
+    }
+    public void QuitBtnClicked()
+    {
+
+    }
+}
