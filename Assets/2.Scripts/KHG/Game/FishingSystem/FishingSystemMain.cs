53b59df7fd3ca6bac7dae727b63f3c3179872375
1_ruf <gyeol0411@naver.com>
2024-08-26 10:06:14 +0900

    AddThrow
    add throw button

---
+++ Assets/2.Scripts/KHG/Game/FishingSystem/FishingSystemMain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FishingSystemMain : MonoBehaviour
+{
+    ItemClass Item = new ItemClass();
+    [SerializeField] private Button throwBtn;
+
+    private int[] magObject = { 0, 1, 2, 3, 4, 5 };
+
+    private bool IsThrowed;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsThrowed)
+            {
+                print("현재 자석 상태 : 던져짐");
+            }
+            else
+            {
+                ThrowBtnClicked();
+            }
+        }
+    }
+    public void ThrowBtnClicked()
+    {
+        throwBtn.interactable = false;
+        MagnetThrowed();
+    }
+    private void MagnetThrowed()
+    {
+
+        
+    }
+    private void SetRandomItem()
+    {
+        int num = Random.Range(0, (magObject.Length - 1));
+        Item.SetItem(num);
+    }
+}
---
+++ Assets/2.Scripts/KHG/Game/FishingSystem/ItemClass.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClass : FishingSystemMain
+{
+    public enum item
+    {
+        clip = 0,
+        vault = 1,
+        glassess = 2
+    }
+
+    private int num;
+    private string name;
+    private float price;
+
+    public void SetItem(int ObjectNum)
+    {
+        switch (item)
+        {
+            case item.
+        }
+    }
+}
